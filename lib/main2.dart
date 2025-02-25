f79dd6aeedbbe8535d89f5147e28d33cae450219
Gemry <gemrydelle@gmail.com>
2025-02-25 21:25:11 +0800

    Initial commit

---
+++ lib/main2.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'To-Do List',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: TodoHomePage(),
+    );
+  }
+}
+
+class TodoHomePage extends StatefulWidget {
+  @override
+  _TodoHomePageState createState() => _TodoHomePageState();
+}
+
+class _TodoHomePageState extends State<TodoHomePage> {
+  final List<Map<String, dynamic>> _tasks = [];
+  final TextEditingController _taskController = TextEditingController();
+
+  void _addTask() {
+    if (_taskController.text.isNotEmpty) {
+      setState(() {
+        _tasks.add({'title': _taskController.text, 'completed': false});
+        _taskController.clear();
+      });
+    }
+  }
+
+  void _toggleTaskCompletion(int index) {
+    setState(() {
+      _tasks[index]['completed'] = !_tasks[index]['completed'];
+    });
+  }
+
+  void _deleteTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('To-Do List')),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _taskController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter a task',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: _addTask,
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    leading: Checkbox(
+                      value: _tasks[index]['completed'],
+                      onChanged: (value) => _toggleTaskCompletion(index),
+                    ),
+                    title: Text(
+                      _tasks[index]['title'],
+                      style: TextStyle(
+                        decoration: _tasks[index]['completed']
+                            ? TextDecoration.lineThrough
+                            : null,
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red),
+                      onPressed: () => _deleteTask(index),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
